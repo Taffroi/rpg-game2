1ba65793ab240628b9604d2ae83f525a6084f8da
Taffroi <taffroi@proton.me>
2025-03-26 16:02:41 +0100

    FIX : Mouse not working in menus

--- scenes/gui/scene_transition/scene_transition.gd
+++ scenes/gui/scene_transition/scene_transition.gd
@@ -2,7 +2,11 @@ extends CanvasLayer
 
 @onready var animation_player: AnimationPlayer = $Control/AnimationPlayer
 
+func _ready() -> void:
+	visible = false
+	
 func fade_out() -> bool:
+	visible = true
 	animation_player.play("fade_out")
 	await animation_player.animation_finished
 	return true
@@ -10,4 +14,5 @@ func fade_out() -> bool:
 func fade_in() -> bool:
 	animation_player.play("fade_in")
 	await animation_player.animation_finished
+	visible = false
 	return true
